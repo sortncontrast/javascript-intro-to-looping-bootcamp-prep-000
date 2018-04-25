7925741e692edffb7aca83411085a0f0b1d5b1b4
tonyamagar@gmail.com <tonyamagar@gmail.com>
2018-04-25 01:39:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,11 @@
-for (let i = 0; i < 25; i++) {
-  let makeArray = [];
-  if(i === 0) {
-    makeArray.push("I am a strange loop.");
-  }else if(i === 1) {
-    makeArray.push("I am 1 strange loop.");
-  } else {
-    makeArray.push("I am \i\ strange loops.");
-  }
+let anArray = [];
+
+for(let i = 0; i < 25; i++) {
+    if(i === 0) {
+      anArray.push("I am a strange loop.");
+    }else if(i === 1) {
+      anArray.push("I am 1 strange loop.");
+    }else {
+      anArray.push("I am \i\ strange loops");
+    }
 }
-console.log(makeArray);
