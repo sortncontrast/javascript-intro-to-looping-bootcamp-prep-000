1c3b85e1d620d4798969891d1cf6923bc819696f
tonyamagar@gmail.com <tonyamagar@gmail.com>
2018-04-25 01:43:42 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,11 @@
 function forLoop(Array) {
   for(i=0; i<25; i++) {
     if(i===0) {
+      Array.push("I am a strange loop.");
+    }else if(i===1) {
+      Array.push("I am 1 strange loop.");
+    }else {
+      Array.push("I am \i\ strange loops.");
+    }
   }
 }
